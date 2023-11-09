a43e7af1642855d7f2f926529512f3c3c85a2f08
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-09 12:46:27 +0600

    jwt security update by checking query eamil and token set email

--- index.js
+++ index.js
@@ -79,14 +79,20 @@ async function run() {
 
     // job related endpoints:
 
+    // which which job posted by the specific user:
     app.get('/postedJobs/:email', verifyToken, async(req, res) => {
       const email = req.params.email;
+      const tokenEmail = req.user.email;
+      if(email !== tokenEmail){
+        return res.status(403).send({message: 'forbidden'});
+      }
       const query = {email: email};
       const cursor = jobCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // one category all jobs (no need protection)
     app.get('/jobs/:categories',  async(req, res) => {
       const categories = req.params.categories;
       const query = {categories: categories};
@@ -95,58 +101,98 @@ async function run() {
       res.send(result);
     })
 
+    // specific one job details(need protection):
     app.get('/postedJobs/find/:id', verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await jobCollection.findOne(query);
+      const query1 = {_id: new ObjectId(id)};
+      const result = await jobCollection.findOne(query1);
       res.send(result);
     });
 
-    app.get('/bids/find/:email',verifyToken, async(req, res) => {
+    // find bids for specific user:(need verification)
+     app.get('/bids/find/:email',verifyToken, async(req, res) => {
       const email = req.params.email;
+      const tokenEmail = req.user.email;
+      if(email !== tokenEmail){
+        return res.status(403).send({message: 'forbidden'});
+      }
       const query = {bidderEmail: email};
       const cursor = bidCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // find bidRequests for specific user:(need verification)
     app.get('/bidRequests/:email', verifyToken, async(req, res) => {
       const email = req.params.email;
+      const tokenEmail = req.user.email;
+      if(email !== tokenEmail){
+        return res.status(403).send({message: 'forbidden'});
+      }
       const query = {buyerEmail: email};
       const cursor = bidCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    // create jobs by users(need verification)
     app.post('/jobs',verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const job = req.body;
       const result = await jobCollection.insertOne(job);
       res.send(result);
     });
 
+    // to bids (needs verification):
     app.post('/bids',verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const bid = req.body;
       const result = await bidCollection.insertOne(bid);
       res.send(result);
     });
 
+    // to comment (no need protection)
     app.post('/comments', async(req, res) => {
       const comment = req.body;
       const result = await commentCollection.insertOne(comment);
       res.send(result);
     })
 
+    // job delete (need verification) by the job owner:
     app.delete('/postedJobs/:id',verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const id = req.params.id;
       console.log(id);
-      const query = {_id: new ObjectId(id)};
-      const result = await jobCollection.deleteOne(query);
+      const query1 = {_id: new ObjectId(id)};
+      const result = await jobCollection.deleteOne(query1);
       res.send(result);
     });
 
     
-
+    // job info updated by the job owner (need verification)
     app.put('/jobs/update/:id',verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const id = req.params.id;
       const updatedInfo = req.body;
       const filter = {_id: new ObjectId(id)};
@@ -165,7 +211,13 @@ async function run() {
       res.send(result);
     });
 
+    // status update by specific user(need verification):
     app.put(`/bidRequests/status/:id`,verifyToken, async(req, res) => {
+      let query = {};
+      if(req.query.email){
+        query = {email: req.query.email};
+      };
+      if(req.query.email !== req.user.email){return res.status(403).send({message: 'forbidden'})};
       const id = req.params.id;
       const {status} = req.body;
       const filter = {_id: new ObjectId(id)};
